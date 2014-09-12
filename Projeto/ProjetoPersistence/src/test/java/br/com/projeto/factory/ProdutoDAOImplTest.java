841c01b5e95b66a0f197de2a181ef3fde318a067
srdelfino <srdelfino@gmail.com>
2014-09-12 15:40:31 -0300

    Atualização do Projeto.

---
+++ Projeto/ProjetoPersistence/src/test/java/br/com/projeto/factory/ProdutoDAOImplTest.java
@@ -0,0 +1,21 @@
+package br.com.projeto.factory;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.projeto.dao.ProdutoDAO;
+import br.com.projeto.dao.ProdutoDAOImpl;
+import br.com.projeto.excecao.PSTException;
+import br.com.projeto.model.Produto;
+
+public class ProdutoDAOImplTest {
+	@SuppressWarnings("unused")
+	@Test
+	@Ignore
+	public void listar() throws PSTException {
+		ProdutoDAO produtoDAO = new ProdutoDAOImpl();
+		List<Produto> lstProdutos = produtoDAO.listar(-1, -1);
+	}
+}
